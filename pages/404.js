26bd0f3523b29c53986a5572031b59069fa53d3e
Gianluca <gianlucalatina@gmail.com>
2021-03-23 17:06:35 +0100

    color fixes

--- pages/404.js
+++ pages/404.js
@@ -10,8 +10,8 @@ export default function Home() {
           <title>404 not found</title>
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
-        <h3 className='mb-4 text-7xl'>404</h3>
-        <h3 className='text-4xl '>
+        <h3 className='mb-4 text-white text-7xl'>404</h3>
+        <h3 className='text-4xl text-white'>
           The page you are looking for does not exist. Click on the link below to go back
           home
         </h3>
